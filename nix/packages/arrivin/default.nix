42decc9baa4881125d412bc87d24662fe73d0e73
Waldemar Tomme <waldetomme@gmail.com>
2025-10-27 12:56:08 +0100

    Add nix packages

---
+++ nix/packages/arrivin/default.nix
@@ -0,0 +1,15 @@
+{
+  buildDotnetModule,
+
+  dotnetCorePackages,
+}:
+buildDotnetModule {
+  pname = "arrivin";
+  version = "0.1";
+  src = ./../../../src;
+
+  projectFile = "Arrivin.Client.Cli/Arrivin.Client.Cli.csproj";
+  nugetDeps = ./deps.json;
+
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+}
---
+++ nix/packages/arrivind/default.nix
@@ -0,0 +1,15 @@
+{
+  buildDotnetModule,
+
+  dotnetCorePackages,
+}:
+buildDotnetModule {
+  pname = "arrivind";
+  version = "0.1";
+  src = ./../../../src;
+
+  projectFile = "Arrivin.Server.Web/Arrivin.Server.Web.csproj";
+  nugetDeps = ./deps.json;
+
+  dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
+}
