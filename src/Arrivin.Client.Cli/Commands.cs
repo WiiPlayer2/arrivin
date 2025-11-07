c379b46aff1b4a409d6965bd3b63508887edd9f4
Waldemar Tomme <waldetomme@gmail.com>
2025-11-07 21:21:21 +0100

    Fix options

--- src/Arrivin.Client.Cli/Commands.cs
+++ src/Arrivin.Client.Cli/Commands.cs
@@ -1,42 +1,16 @@
 using System.CommandLine;
-using Arrivin.Domain;
 
 namespace Arrivin.Client.Cli;
 
 public static class Commands
 {
-    public static RootCommand Root => new()
+    public static Command Deploy { get; } = new("deploy")
     {
-        Options.Server,
-        Get,
-        Set,
-        Push,
-        Pull,
-        Publish,
-        Deploy,
+        Arguments.DeploymentName,
+        Arguments.ExtraArgs,
     };
 
     public static Command Get { get; } = new("get")
-    {
-        Arguments.DeploymentName,
-    };
-
-    public static Command Set { get; } = new("set")
-    {
-        Arguments.DeploymentName,
-        Options.Store,
-        Options.Derivation,
-        Options.OutPath,
-    };
-
-    public static Command Push { get; } = new("push")
-    {
-        Arguments.DeploymentName,
-        Arguments.Path,
-        Options.Store,
-    };
-
-    public static Command Pull { get; } = new("pull")
     {
         Arguments.DeploymentName,
     };
@@ -48,9 +22,35 @@ public static class Commands
         Arguments.ExtraArgs,
     };
 
-    public static Command Deploy { get; } = new("deploy")
+    public static Command Pull { get; } = new("pull")
     {
         Arguments.DeploymentName,
-        Arguments.ExtraArgs,
+    };
+
+    public static Command Push { get; } = new("push")
+    {
+        Arguments.DeploymentName,
+        Arguments.Path,
+        Options.Store,
+    };
+
+    public static RootCommand Root => new()
+    {
+        Options.Server,
+        Options.DataDirectory,
+        Get,
+        Set,
+        Push,
+        Pull,
+        Publish,
+        Deploy,
+    };
+
+    public static Command Set { get; } = new("set")
+    {
+        Arguments.DeploymentName,
+        Options.Store,
+        Options.Derivation,
+        Options.OutPath,
     };
 }
--- src/Arrivin.Client.Cli/Options.cs
+++ src/Arrivin.Client.Cli/Options.cs
@@ -9,13 +9,13 @@ public static class Options
         DefaultValueFactory = _ => new DirectoryInfo("/var/lib/arrivin"),
     };
 
-    public static Option<string> Derivation { get; } = new("derivation", "-d", "--derivation");
+    public static Option<string> Derivation { get; } = new("--derivation", "-d");
 
-    public static Option<bool> IgnorePushErrors { get; } = new("ignore-push-errors", "--ignore-push-errors");
+    public static Option<bool> IgnorePushErrors { get; } = new("--ignore-push-errors");
 
-    public static Option<string> OutPath { get; } = new("out-path", "-o", "--out-path");
+    public static Option<string> OutPath { get; } = new("--out-path", "-o");
 
-    public static Option<string> Server { get; } = new("server", "-u", "--server");
+    public static Option<string> Server { get; } = new("--server", "-u");
 
-    public static Option<string> Store { get; } = new("store", "-s", "--store");
+    public static Option<string> Store { get; } = new("--store", "-s");
 }
