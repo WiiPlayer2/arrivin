70d31c07e94a8203da64f4462c53cd48b1662508
Waldemar Tomme <waldetomme@gmail.com>
2025-11-08 21:27:14 +0100

    Only actually publish if repository was updated

--- nix/nixosModules/default/run-publish.sh
+++ nix/nixosModules/default/run-publish.sh
@@ -8,12 +8,17 @@ shift
 shift
 JOBS="$*"
 
+_old_commit=""
 if [ ! -d "$REPO_PATH" ]; then
     git clone "$REMOTE" "$REPO_PATH"
 else
+    _old_commit=$(git rev-parse HEAD)
     git -C "$REPO_PATH" pull
 fi
+_new_commit=$(git rev-parse HEAD)
 
-for job in $JOBS; do
-    arrivin --server "$URL" publish "$REPO_PATH#arrivin.$job" "$IGNORE_PUSH_ERRORS_ARG" -- --max-jobs 1 || true
-done
+if [[ "$_old_commit" != "$_new_commit" ]]; then
+    for job in $JOBS; do
+        arrivin --server "$URL" publish "$REPO_PATH#arrivin.$job" "$IGNORE_PUSH_ERRORS_ARG" -- --max-jobs 1 || true
+    done
+fi
